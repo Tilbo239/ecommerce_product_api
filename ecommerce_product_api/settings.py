176681774bc5e7660f50a3d0e25e76fe7925afb2
Tilbo239 <roaimtilbo6363@gmail.com>
2025-03-24 17:45:58 +0100

    users app created

--- ecommerce_product_api/settings.py
+++ ecommerce_product_api/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',  # Django REST framework
+    'rest_framework_simplejwt',# Django REST framework Simple JWT
+    'users',
+    
 ]
 
 MIDDLEWARE = [
@@ -129,3 +133,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication', # JWT Authentication
+        'rest_framework.authentication.SessionAuthentication', # Session Authentication
+        
+    ]
+}
+
+
+AUTH_USER_MODEL = 'users.CustomUser'
