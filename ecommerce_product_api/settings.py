5ad0ed56995917e53820328639455c5e503ccd8d
Tilbo239 <roaimtilbo6363@gmail.com>
2025-03-24 16:39:03 +0100

    database configuration: mysql

--- ecommerce_product_api/settings.py
+++ ecommerce_product_api/settings.py
@@ -74,9 +74,17 @@ WSGI_APPLICATION = 'ecommerce_product_api.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_api_ecommerce',
+        'USER': 'root',
+        'PASSWORD': 'ITServ32$',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",  # Helps prevent SQL injection
+        }
     }
 }
 
