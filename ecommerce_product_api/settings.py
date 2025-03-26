d9e4b075ca1fd1388e22dec4a9a5ddc9a0da2079
Tilbo239 <roaimtilbo6363@gmail.com>
2025-03-26 12:50:52 +0100

    review app created

--- ecommerce_product_api/settings.py
+++ ecommerce_product_api/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',# Django REST framework Simple JWT
     'users', # The users app
     'products', # he products apps
+    'reviews', # The reviews app
 
 ]
 
