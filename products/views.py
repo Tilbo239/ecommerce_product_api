13b3ab3d19c88f131811cce8a05a6c02a897bfb1
Tilbo239 <roaimtilbo6363@gmail.com>
2025-03-26 15:53:34 +0100

    permissions added to the view of products and reviews app

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly 
+from permi.permissions import IsAdminOrReadOnly
 from .models import Product, Category, ProductImage
 from .serializers import  ProductImageSerializer, ProductSerializer, CategorySerializer
 
@@ -10,14 +12,17 @@ class CategoryViewSet(viewsets.ModelViewSet):
   
   queryset = Category.objects.all()
   serializer_class = CategorySerializer
+  permission_classes = [IsAdminOrReadOnly]
 
 class ProductViewSet(viewsets.ModelViewSet):
   
   queryset = Product.objects.all()
   serializer_class = ProductSerializer
+  permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class ProductImageViewSet(viewsets.ModelViewSet):
   queryset = ProductImage.objects.all()
   serializer_class = ProductImageSerializer
+  permission_classes = [IsAuthenticatedOrReadOnly]
 
--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,7 @@
 from rest_framework import generics
+
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from permi.permissions import IsOwnerOrReadOnly, IsAdminOrReadOnly
 from rest_framework.validators import ValidationError
 from .models import Review
 from rest_framework.response import Response
@@ -6,10 +9,16 @@ from products.models import Product
 from .serializers import  ReviewSerializer
 
 
+
 class CreateReview(generics.CreateAPIView):
+  """
+Class CreateReview: A view for creating a new review for a product.
+
+"""
   
   queryset = Review.objects.all()
   serializer_class = ReviewSerializer
+  permission_classes = [IsAuthenticatedOrReadOnly]
 
   def perform_create(self, serializer):
     pk = self.kwargs.get('pk')
@@ -19,11 +28,15 @@ class CreateReview(generics.CreateAPIView):
     review_queryset = Review.objects.filter(product=product, user=review_user)
 
     if review_queryset.exists():
-      raise ValidationError("You have already reviewed this product!")
+      raise ValidationError("You can't review the same product twice.")
 
     serializer.save(product=product, user=review_user)
   
 class ProductReviewList(generics.ListAPIView):
+  """
+Class:
+    ProductReviewList: A view for listing all reviews for a specific product.
+"""
     
   serializer_class = ReviewSerializer
 
@@ -34,12 +47,21 @@ class ProductReviewList(generics.ListAPIView):
 
 
 class ReviewList(generics.ListAPIView):
+  """
+    Class ReviewList: A view for listing all reviews.
+  """
   
   queryset = Review.objects.all()
   serializer_class = ReviewSerializer
 
 class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
-  
+  """
+    Class ReviewDetail: A view for retrieving, updating, or deleting a specific review.
+  """
   queryset = Review.objects.all()
   serializer_class = ReviewSerializer
+  permission_classes = [IsOwnerOrReadOnly]
+
+
+
 
